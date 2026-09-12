3a3d964d19716b428c6f6a6d8ecf5f2ca289c189
premananda <solamiotender@gmail.com>
2026-09-12 19:50:11 +0530

    added Url

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const config = {
   port: Number(process.env.PORT || 5000),
   mongoUri: process.env.MONGO_URI || 'mongodb+srv://mombabycare21_db_user:fHxuoQWbhPn4PpgE@cluster0.muk5veg.mongodb.net/MABC',
   mongoBackupUri: process.env.MONGODB_BACKUP || 'mongodb+srv://rajeshbsahoo_db_user:evtJDQypmnQvzjJ1@cluster0.8b9riqv.mongodb.net/MABC_BACKUP_DATA',
-  clientUrl: process.env.CLIENT_URL || 'https://www.mombabycare.co.in',
+  clientUrl: process.env.CLIENT_URL || 'https://www.mombabycare.co.in' || 'https://mombabycare.co.in',
   adminUrl: process.env.ADMIN_URL || 'https://admin.mombabycare.co.in',
   cloudinary: {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
